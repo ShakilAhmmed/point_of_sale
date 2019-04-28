5f71338f759179369d83eeedfde5023b7fc5152d
Shakil Ahmmed <shakilfci461@gmail.com>
2019-04-29 01:02:10 +0600

    Dashboard

--- api_data/urls.py
+++ api_data/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('get_stock_data', views.get_stock_data, name="get_stock_data"),
     path('get_purchase_data', views.get_purchase_data, name="get_purchase_data"),
     path('get_sale_data', views.get_sale_data, name="get_sale_data"),
+    path('get_product_data', views.get_product_data, name="get_product_data"),
 ]
